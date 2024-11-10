ff3b2a08087359bf9b7d449523869cba1a9977b8
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-11-10 21:52:18 +0200

    Trying to get search working

--- app/apiFunctions.ts
+++ app/apiFunctions.ts
@@ -5,17 +5,37 @@ export async function fetchBooks(userInput: string) {
 		q: `Star Wars ${userInput}`,
 		printType: "books",
 		key: process.env.BOOKS_API_KEY || "",
+		fields: "totalItems,items/volumeInfo",
 	});
 
 	const url = new URL(baseUrl);
 	url.search = params.toString();
 
 	try {
-		const res = await fetch(url);
+		const res = await fetch(url, {
+			headers: {
+				"Accept-Encoding": "gzip",
+				"User-Agent": "sw-book-finder (gzip)",
+			},
+		});
 		const data = await res.json();
-		return data;
+
+		console.log(data);
+
+		const books = [];
+		for (let i = 0; i < data.items.length; i++) {
+			const volumeInfo: any = data.items[i].volumeInfo;
+
+			if (!volumeInfo) {
+				continue;
+			}
+
+			books.push(volumeInfo);
+		}
+
+		return { totalItems: data.totalItems, bookData: books };
 	} catch (error) {
 		console.error(error);
-		return {};
+		throw new Response("Something went wrong!", { status: 500 });
 	}
 }
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,5 +1,11 @@
-import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
+import type {
+	MetaFunction,
+	LoaderFunctionArgs,
+	ActionFunctionArgs,
+} from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { fetchBooks } from "~/apiFunctions";
+import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 
 export const meta: MetaFunction = () => {
@@ -9,16 +15,39 @@ export const meta: MetaFunction = () => {
 	];
 };
 
-// export async function loader({ request }: LoaderFunctionArgs) {
-// 	const data = await fetchBooks("Bane");
-// 	console.log(data);
-// 	return null;
-// }
+export async function loader({ request }: LoaderFunctionArgs) {
+	const url = new URL(request.url);
+	const q = url.searchParams.get("q");
+
+	if (!q) {
+		return { bookData: null };
+	}
+
+	const books = await fetchBooks(q);
+	console.log(books);
+
+	return { bookData: books, q };
+}
 
 export default function Index() {
+	const { bookData, q } = useLoaderData<typeof loader>();
 	return (
-		<div>
-			<Input type="text" placeholder="Search for a Star Wars book" />
-		</div>
+		<Form
+			className="items-center flex space-x-2 mt-16 justify-center w-screen px-3"
+			id="search-form"
+			role="search"
+		>
+			<Input
+				type="search"
+				defaultValue={q || ""}
+				id="q"
+				name="q"
+				placeholder="Search for a Star Wars book"
+				className="max-w-3xl"
+			/>
+			<Button className="hidden md:inline-flex" type="submit">
+				Search
+			</Button>
+		</Form>
 	);
 }
